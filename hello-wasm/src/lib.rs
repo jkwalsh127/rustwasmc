4817ca4a498822eb113af9e55648309d198c375b
Jake Walsh <jkwalsh127@gmail.com>
2022-09-14 21:59:39 -0700

    import book folder

--- hello-wasm/src/lib.rs
+++ hello-wasm/src/lib.rs
@@ -10,12 +10,11 @@ pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
 }
 
-// must make fxn public so that it can be called from other files
-// when returning a value, rust will take the last statement in the function and return that (expression-oriented language)
 #[wasm_bindgen]
 pub fn add_numbers(n1: i32, n2:i32) -> i32{
     n1 + n2
 }
+
 #[wasm_bindgen]
 pub fn square(n1: i32) -> i32{
     n1*n1
